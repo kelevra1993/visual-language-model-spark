45e6faae21d773322f6b911aeb39dc549ef641c0
Robert Kelevra <kelevra.robert@gmail.com>
2026-09-08 17:45:27 +0200

    [ARCHITECTURE] Update model to instantiate Backbone properly.

--- object_detection/model/model.py
+++ object_detection/model/model.py
@@ -1,31 +1,41 @@
 import torch
-
 from torch import nn
+from typing import Dict, Any
 from architecture_modules.backbone import Backbone
 
+
 class Model(nn.Module):
+    """
+    Constructs the overall object detection model by combining the backbone,
+    region proposal, enhancer, and detector modules based on the configuration.
+    """
 
-    def __init__(self, configuration: dict, device: torch.device = None, dtype: torch.dtype = None):
+    def __init__(self, configuration: Dict[str, Any], device: torch.device = None, dtype: torch.dtype = None) -> None:
         """
-        Initializes the model using a configuration dictionary.
-        # todo we will later describe the model with Backbone, RPN, Enhancer...e.t.c
-
+        Initializes the Model components.
+        
         Args:
-            configuration (dict): A dictionary parsed from a YAML configuration file.
-            device (torch.device, optional): Device on which the model should run.
-            dtype (torch.dtype, optional): Data type of the model.
+            configuration (Dict[str, Any]): The complete configuration dictionary for the experiment.
+            device (torch.device): The device for parameter allocation.
+            dtype (torch.dtype): The data type for parameter allocation.
         """
-        super().__init__()
+        super(Model, self).__init__()
 
+        # Assign device and dtype globally for the model
         self.device = device
         self.dtype = dtype
 
         # Get configuration for each component of our model
+        data_configuration = configuration.get('Data', {})
         backbone_configuration = configuration.get('Backbone', {})
 
-
-        # # Set up the backbone
-        # self.backbone = Backbone(
-        #     # todo to be filled
-        #     device=self.device,
-        #     dtype=self.dtype)
+        # Set up the backbone feature extractor using the specified configurations
+        self.backbone = Backbone(
+            input_channels=backbone_configuration.get("input_channels"),
+            convolutions=backbone_configuration.get("convolutions"),
+            modules=backbone_configuration.get("modules"),
+            last_max_pooling=backbone_configuration.get("last_max_pooling"),
+            normalization=backbone_configuration.get("normalization"),
+            device=self.device,
+            dtype=self.dtype
+        )
