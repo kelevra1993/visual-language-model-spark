c6d3fba4dac0043052aa8ee41137ab7109c36d20
Robert Kelevra <kelevra.robert@gmail.com>
2026-09-18 16:56:24 +0200

    [UTILITIES] Update configuration checker comments to reference detection terminology.

--- object_detection/utilities/model/configuration_checker.py
+++ object_detection/utilities/model/configuration_checker.py
@@ -1,5 +1,5 @@
 
 # verify consistency of thresholds for nms and foreground and background settings
-# Make sure the choice of the enhancers are coherent across anchors, and enhancer information.
+# Make sure the choice of the detections are coherent across anchors, and detection information.
 # Make sure image size is an exponent such of 2. So that we do not have issues with the pooling layers in terms of keeping track of downsampling.
 # Make sure scales are integers
