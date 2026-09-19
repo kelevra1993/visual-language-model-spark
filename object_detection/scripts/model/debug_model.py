6a5de5673db2ba48fa79bcccd7dcc5c15d2b1424
Robert Kelevra <kelevra.robert@gmail.com>
2026-09-19 14:07:24 +0200

    [DEBUG] Add dummy ground truth labels to model debugger.

--- object_detection/scripts/model/debug_model.py
+++ object_detection/scripts/model/debug_model.py
@@ -6,7 +6,7 @@ from model.model import Model
 import numpy as np
 from scripts.utilities.debugging_utilities import print_bounding_boxes, generate_random_bounding_boxes, \
     visualize_anchor_target_assignments, visualize_foreground_and_background_anchors, get_model_configuration, \
-    get_dummy_input_tensor, get_dummy_ground_truth_boxes
+    get_dummy_input_tensor, get_dummy_ground_truth_boxes, get_dummy_ground_truth_labels
 from utilities.os_utilities import print_green, print_blue
 from utilities.tensor_utilities import print_tensor_status, print_tensor_shape, print_tensor_list
 
@@ -19,6 +19,7 @@ def debug_model() -> None:
     the outputs of the backbone and subsequently the region proposals.
     """
 
+    number_classes = 21  # Standard (20 objects + 1 background)
     configuration, device, dtype = get_model_configuration()
 
     model = Model(configuration=configuration, mode="training", device=device, dtype=dtype)
@@ -33,6 +34,11 @@ def debug_model() -> None:
                                                                configuration=configuration,
                                                                device=device,
                                                                dtype=dtype)
+                                                               
+    ground_truth_labels = get_dummy_ground_truth_labels(ground_truth_boxes=ground_truth_bounding_boxes,
+                                                        number_classes=number_classes,
+                                                        device=device,
+                                                        dtype=dtype)
 
     print_blue(output="Visualising Ground Truths with Possitive and Negative Anchors...", add_separators=True)
     # Only run for the first batch to visualise the ground truth iou's with proposals.
@@ -46,7 +52,9 @@ def debug_model() -> None:
     for index, boxes in enumerate(ground_truth_bounding_boxes):
         print_tensor_shape(tensor=boxes, name=f"ground_truth_boxes_image_{index}", indent=1)
 
-    model_output_dictionary = model(input_tensor=input_tensor, ground_truth_bounding_boxes=ground_truth_bounding_boxes)
+    model_output_dictionary = model(input_tensor=input_tensor, 
+                                    ground_truth_bounding_boxes=ground_truth_bounding_boxes,
+                                    ground_truth_labels=ground_truth_labels)
 
 
 if __name__ == "__main__":
